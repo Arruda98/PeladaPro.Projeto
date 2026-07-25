83984364ca4de5ac02fb52d579da0beca41b2f55
Arruda98 <jeffersonarruda98@gmail.com>
2026-07-25 09:21:00 -0300

    Iniciando a logica

---
+++ PeladaPro.Projeto/Jogador.cs
@@ -0,0 +1,23 @@
+﻿namespace PeladaPro.Projeto
+{
+    internal class Jogador
+    {
+        private string _name { get; set; }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("O nome não pode ser em branco");
+                _name = value; 
+            }
+        }
+
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public int SkillLevel { get; set; }
+
+    }
+}
